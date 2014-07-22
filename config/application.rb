b59a454127c356c7812171866b515bf599a3fcd4
Sorokin Vladimir <sorokinva.spb@gmail.com>
2014-07-22 22:17:02 +0400

    fix error in application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,16 @@ module SigmaProject
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.encoding = "utf-8"    
+    
     config.generators do |g|
-      g.test_framework :rspec, :view_specs => false, :fixture => true, fixture_replacement => "factory_girl"
       g.assets = false
       g.helper = false
+      g.test_framework :rspec, :fixture => true
+      g.view_specs false
+      g.helper_specs false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
   end
 end
+
