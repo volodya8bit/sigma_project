bf3ca92e59f93d5088b5cb82fac223103358a01a
logicall <shchepyotkin@gmail.com>
2014-07-29 16:24:59 +0400

    add column categories_id in recipes table

--- db/migrate/20140729083617_create_recipes.rb
+++ db/migrate/20140729083617_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      # t.integer :id
+      t.integer :categories_id
       t.text :text
       t.boolean :published
       t.text :ingredient
