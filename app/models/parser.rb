4a7fa8ddbbaa3f6f5bb7da847cf1d37bb5c9ba74
logicall <shchepyotkin@gmail.com>
2014-07-30 13:50:59 +0400

    change spec and models

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,3 +1,3 @@
 class Parser < ActiveRecord::Base
-  has_many :recipe
+  has_many :recipes
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,7 @@ describe Category do
   it "has a valid factory" do
     expect(FactoryGirl.build(:category)).to be_valid
   end
+
+  it {is_expected.to have_many(:favorites)}
+
 end
