72d45a24d1290040a1520d3e08552ba453fff9a4
Sorokin Vladimir <sorokinva.spb@gmail.com>
2014-08-03 22:19:10 +0400

    new CategoriesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
   devise_for :users
   resources :categories
 
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, :type => :controller do
 
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
 end
