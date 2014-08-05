780116aab3c07786a8b72f6c137f504e1bda6536
logicall <shchepyotkin@gmail.com>
2014-08-05 10:27:29 +0400

    refactors in views and controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,14 @@
-class CategoriesController < ApplicationController  
-    
+class CategoriesController < ApplicationController
+
+  before_action :find_category, only: [:edit, :show]
+
+
   def index
     #@categories = Category.all
-    @categories = Category.paginate( :page => params[:page], :per_page => 5)
+    @categories = Category.paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
-   @category = Category.find(params[:id])
   end
 
   def new
@@ -20,10 +22,10 @@ class CategoriesController < ApplicationController
     else
       render :new
     end
+
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
@@ -40,38 +42,9 @@ class CategoriesController < ApplicationController
     @category.destroy
     redirect_to categories_path
   end
-    
-  def show
-    @category = Category.find(params[:id])
-  end
-  def new
-    @category = Category.new
-  end
-  def create
-    @category = Category.new(params.require(:category).permit(:title))
-    if @category.save
-      redirect_to categories_path
-    else
-      render :new
-    end
- 
-  end
 
-  def edit
+  private
+  def find_category
     @category = Category.find(params[:id])
-  end
-  
-  def update
-    @category = Category.find(params[:id])
-    if @category.update(params.require(:category).permit(:title))
-      redirect_to categories_path
-    else
-      render :edit
-    end
-  end
-  def destroy
-    @category = Category.find(params[:id])
-    @category.destroy
-    redirect_to categories_path
   end
 end
