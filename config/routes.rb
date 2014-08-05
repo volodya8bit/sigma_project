1b59274613a4a424541ce05bc85713948b6e92a0
logicall <shchepyotkin@gmail.com>
2014-08-05 10:55:04 +0400

    change in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-
-  get 'categories/show'
-  
-  get 'categories/new'
-  
-  get 'categories/edit'
   devise_for :users
 
-  resources :categories
+  resources :categories, only: [:index, :show, :new, :edit]
+  # get 'categories/index'
+  #
+  # get 'categories/show'
+  #
+  # get 'categories/new'
+  #
+  # get 'categories/edit'
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
