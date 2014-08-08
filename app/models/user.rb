0d6d1270aad1654177f659ea73025345b9e2a087
Илья Щ <logic@macbook-pro-ila.netgear.com>
2014-08-09 00:02:38 +0400

    remov validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   enumerize :role, in: {user: 1, admin: 2}, default: :user
 
   validates :email, :password, presence: true
-  validates :email, uniqueness: true
+  # validates :email, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "is not an e-mail format" }
   validates :password, length: { in: 6..20 }
 end
