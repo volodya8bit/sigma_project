54960d59ff3fedce00aabe16de6485575ba41bac
Илья Щ <logic@macbook-pro-ila.netgear.com>
2014-08-10 17:56:58 +0400

    fix rspec

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :recipe do
-    text { Faker::Lorem.word }
+    title { Faker::Lorem.word }
     text { Faker::Lorem.paragraph(4) }
     published { [true, false].sample }
     ingredient { Faker::Lorem.sentence }
