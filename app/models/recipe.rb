5e59e34a600575a100c262638469f332cd74474d
Илья Щ <logic@macbook-pro-ila.netgear.com>
2014-08-10 18:54:40 +0400

    change search form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ActiveRecord::Base
   belongs_to :category
   belongs_to :parser
   has_many :favorites
+
+  def self.search(query)
+    where("title like ?", "%#{query}%")
+  end
+
 end
