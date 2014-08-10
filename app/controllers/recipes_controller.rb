d612eaeb785e18603e7f7a51b7851edf3326dc2c
Илья Щ <logic@macbook-pro-ila.netgear.com>
2014-08-10 12:10:02 +0400

    add controller recipes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,58 @@
+class RecipesController < ApplicationController
+
+  before_action :find_recipe, only: [:edit, :show, :update, :destroy]
+  load_and_authorize_resource param_method: :parser_params
+
+
+  def index
+    if params[:search]
+      @recipes = Recipe.search(params[:search]).order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+    else
+      @recipes = Recipe.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+    end
+
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(parser_params)
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:text)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :categories
   resources :parsers
+  resources :recipes
 
 
 
