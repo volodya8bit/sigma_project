a047614b3412e27cf7c42ae135eedfd81f2be3c3
Илья Щ <logic@macbook-pro-ila.netgear.com>
2014-08-10 18:13:15 +0400

    change migrations

--- db/migrate/20140729083617_create_recipes.rb
+++ db/migrate/20140729083617_create_recipes.rb
@@ -3,6 +3,7 @@ class CreateRecipes < ActiveRecord::Migration
     create_table :recipes do |t|
       t.integer :category_id
       t.integer :parser_id
+      t.string :title
       t.text :text
       t.boolean :published
       t.text :ingredient
